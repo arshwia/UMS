e0bbb3513f95b42b5404ade07ef18b937bd65349
arshwia <arshia.hy.coding@gmail.com>
2025-04-20 19:39:40 +0330

    add  style login paeg

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const { title } = require('process');
 
 const dbURI = "mongodb+srv://arshia:arshia1234@cluster0.wnrotdp.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0"
 
@@ -18,3 +19,9 @@ app.use(morgan("dev"));
 // mongoose.connect(dbURI)
 //     .then(() => console.log("con"))
 //     .catch((err) => console.log(err))
+
+app.listen(3000)
+
+app.get("/", (req, res) => {
+	res.render("login", { title: "login" })
+})
