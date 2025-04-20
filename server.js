44b10669900b4d0929a48f1909562e18981a3674
arshwia <arshia.hy.coding@gmail.com>
2025-04-20 09:05:41 +0330

    ejs code and server js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const dbURI = "mongodb+srv://arshia:arshia1234@cluster0.wnrotdp.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0"
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+// mongoose.connect(dbURI)
+//     .then(() => console.log("con"))
+//     .catch((err) => console.log(err))
